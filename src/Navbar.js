bf8dc460c6ca732ab699039df60c5c51ce76eed1
julianasinai <julianasinai1@gmail.com>
2020-01-25 22:49:14 -0300

    Add PaletteList and Links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
@@ -26,7 +30,7 @@ class Navbar extends Component {
     return(
       <header className='Navbar'>
         <div className='logo'>
-          <a href='#'>reactcolorpicker</a>
+          <Link to='/'>reactcolorpicker</Link>
         </div>
         <div  className='slider-container'>
           <span>Level: {this.props.level}</span>
