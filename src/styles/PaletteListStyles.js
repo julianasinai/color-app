3e3ac520473a9e14b98bcfdd26f4e240a23a3e4b
julianasinai <julianasinai1@gmail.com>
2020-02-19 20:51:04 -0300

    Animate palette deletion

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -2,11 +2,21 @@ import sizes from './sizes';
 import bg from './bg.svg';
 
 export default {
+  '@global': {
+    '.fade-exit': {
+      opacity: 1
+    },
+    '.fade-exit-active': {
+      opacity: 0,
+      transition: 'opacity 500ms ease-out'
+    }
+  },
   root: {
     height: '100vh',
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'center',
+    /* background by SVGBackgrounds.com */
     backgroundColor: '#394bad',
     backgroundImage: `url(${bg})`,
     overflow: 'scroll'
