4b24ff90a7a832d49ebb025a0e7221783c16d20b
julianasinai <julianasinai1@gmail.com>
2020-02-19 20:12:21 -0300

    Make PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,17 +1,26 @@
+import sizes from './sizes';
+
 export default {
   root: {
     backgroundColor: 'blue',
     height: '100vh',
     display: 'flex',
-    alighItems: 'flex-start',
+    alignItems: 'flex-start',
     justifyContent: 'center'
+  
   },
   container: {
     width: '50%',
     display: 'flex',
     alignItems: 'flex-start',
     flexDirection: 'column',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    [sizes.down('xl')]: {
+      width: '80%'
+    },
+    [sizes.down('xs')]: {
+      width: '75%'
+    }
   }, 
   nav: {
     display: 'flex',
@@ -28,6 +37,13 @@ export default {
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%'
+    gridGap: '2.5rem',
+    [sizes.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)'
+    },
+    [sizes.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '1rem'
+    }
   }
 }
